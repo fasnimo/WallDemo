0d07f026091f0261e820b926fcfe77b98cda4b48
fasnimo <fasnimo@gmail.com>
2022-12-19 16:51:23 -0600

    update to comments addeding list and remove

--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './Components/App'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react'
+import ReactDOM from 'react-dom'
 import './styles/stylesheet.css'
+import {BrowserRouter} from 'react-router-dom'
+import {createStore} from 'redux'
+import rootReducer from './redux/reducer'
 import {Provider} from 'react-redux'
-import { createStore } from 'redux';
-import rootReducer from './redux/reducer';
+import App from './Components/App'
 
-const store = createStore(rootReducer)
-
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 
-
-ReactDOM.render(<Provider store={store}>
-    <BrowserRouter>
-    <App/>
-    </BrowserRouter>
-    </Provider>,
-     document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><BrowserRouter><App/></BrowserRouter></Provider>, document.getElementById('root'))
